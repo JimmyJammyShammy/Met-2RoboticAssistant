265f77df6f03a1c67f5b2d3c28f6cd336e4f2641
Justin Tran <jimmyjammyshammy@gmail.com>
2026-02-02 20:48:32 -0500

    Main File v1 Added

---
+++ Met^2RobotAssistantMain-v1.cpp
@@ -0,0 +1,13 @@
+/*
+    Project: Met^2RobotAssistantMain-v1
+    File:    Met^2RobotAssistant-v1.cpp
+    Author:  Justin Tran
+    Created: 02/02/2026
+    Version: 1.0.0
+    Description:
+        Version 1 of robot control code for Met^2 Robotic Assitant Project 2026.
+*/
+
+int main(int argc, char *argv[]) {
+       
+}
