5fe350d1a1dd34dfc484e9b29ad615d8b7d04dac
Justin Tran <jimmyjammyshammy@gmail.com>
2026-02-03 18:27:20 -0500

    Setup project for micropython development on ESP32

--- Met^2RobotAssistantMain-v1.py
+++ Met^2RobotAssistantMain-v1.py
@@ -7,12 +7,39 @@
     Description:
         Version 1 of robot main code for Met^2 Robotic Assitant Project 2026.
 """
-
+# Allow for immutable variable type
+from typing import final
+# MicroPython imports
+from machine import Pin
+from utime import sleep
 class RoboticAssistant:
     
+# To Run: Connect ESP32 > run > MicroPico > Run current file on Pico
+# VSCode: Press Run at the bottom next to Reset
+# Look at MicroPico for more documentation
+
     """Main class for the Met^2 Robotic Assistant."""
     def main():
         print("Robotic Assistant 2026")
+
+
+        # Setup LED pin to flash
+        pin = Pin("LED", Pin.OUT)
+
+        print("LED starts flashing...")
+        while True:
+            try:
+                pin.value(not pin.value())
+                sleep(1) # sleep 1sec
+            except KeyboardInterrupt:
+                break
+        pin.off()
+        print("Finished.")
+
+
+
+        
+
 
     main()
 
