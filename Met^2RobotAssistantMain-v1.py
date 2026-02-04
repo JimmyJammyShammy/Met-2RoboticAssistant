774ab0f463631e0ae268975235df85aec060af3c
Justin Tran <jimmyjammyshammy@gmail.com>
2026-02-03 19:11:41 -0500

    Added MicroPython imports and basic code

--- Met^2RobotAssistantMain-v1.py
+++ Met^2RobotAssistantMain-v1.py
@@ -7,22 +7,37 @@
     Description:
         Version 1 of robot main code for Met^2 Robotic Assitant Project 2026.
 """
-# Allow for immutable variable type
-from typing import final
 # MicroPython imports
-from machine import Pin
-from utime import sleep
+from machine import Pin # Pin class for GPIO control
+from machine import Timer # Timer class for timing functions
+from machine import ADC # ADC class for analog to digital conversion
+from machine import SoftSPI # Software SPI communication (Serial Peripheral Interface)
+from machine import SPI # Hardware SPI communication
+from machine import SoftI2C # Software I2C communication (Inter-Integrated Circuit)
+from machine import I2C # Hardware I2C communication
+from machine import RTC # Real Time Clock class for trachking time
+from machine import DEEPSLEEP # Deep sleep mode for turning off the robot
+from utime import sleep # Sleep function for delays
+
+
 class RoboticAssistant:
     
 # To Run: Connect ESP32 > run > MicroPico > Run current file on Pico
 # VSCode: Press Run at the bottom next to Reset
 # Look at MicroPico for more documentation
 
+# Pin modes
+    O = Pin.OUT 
+    I = Pin.IN
+
+# Pin Definitions 0-19, 21-23, 25-27, 32-39, Check Board documentation for specific names
+    # Pass in pin number and mode
+    p0 = Pin(0, O)  # Output on GPIO0 (Pin 0)
+    LED = Pin("LED", O)  # Onboard LED pin
+
+
     """Main class for the Met^2 Robotic Assistant."""
     def main():
-        print("Robotic Assistant 2026")
-
-
         # Setup LED pin to flash
         pin = Pin("LED", Pin.OUT)
 
@@ -35,6 +50,7 @@ class RoboticAssistant:
                 break
         pin.off()
         print("Finished.")
+        
 
 
 
